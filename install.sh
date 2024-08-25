85faa9ad13de80031fb6d3e88056a785cee3040a
Carl Schader <carlschader@gmail.com>
2024-08-25 16:52:27 -0500

    install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# This script will install the grep-kill.sh script in the /usr/local/bin directory
+
+
+sudo curl "https://raw.githubusercontent.com/CarlSchader/grep-kill/main/grep-kill.sh" -o /usr/local/bin/grep-kill
+
+sudo chmod +x /usr/local/bin/grep-kill
+
